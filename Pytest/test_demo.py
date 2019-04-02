88ffbc5e061db3ee6b95808e1f6f0e518e2a4348
liqhe <378889804@qq.com>
2019-04-03 00:13:01 +0800

    更新

--- Pytest/test_demo.py
+++ Pytest/test_demo.py
@@ -7,4 +7,13 @@ class TestClass:
         return x + 1
 
     def test_func(self):
+        print('测试')
         assert self.func(3) == 5
+
+    def test_one(self):
+        x = "this"
+        assert 'h' in x
+
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, 'check')
